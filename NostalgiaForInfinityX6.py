be517c001f05fa84eccd9ce5a224ef9117649391
root <root@fraqtrade.home>
2025-06-04 11:00:14 +0300

    Updated strategy 2025-06-04 11:00:14

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -70,7 +70,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.5.73"
+    return "v16.5.74"
 
   stoploss = -0.99
 
@@ -28512,7 +28512,7 @@ class NostalgiaForInfinityX6(IStrategy):
 
     if (
       (self.grinding_v2_grind_1_enable)
-      and (is_derisk_1_found or is_derisk_2_found or is_derisk_3_found)
+      # and (is_derisk_1_found or is_derisk_2_found or is_derisk_3_found)
       and is_long_grind_entry
       and (current_time - timedelta(minutes=5) > filled_entries[-1].order_filled_utc)
       and ((current_time - timedelta(hours=2) > filled_orders[-1].order_filled_utc) or (slice_profit < -0.02))
@@ -28606,7 +28606,7 @@ class NostalgiaForInfinityX6(IStrategy):
 
     if (
       (self.grinding_v2_grind_2_enable)
-      and (is_derisk_1_found or is_derisk_2_found or is_derisk_3_found)
+      # and (is_derisk_1_found or is_derisk_2_found or is_derisk_3_found)
       and is_long_grind_entry
       and (current_time - timedelta(minutes=5) > filled_entries[-1].order_filled_utc)
       and ((current_time - timedelta(hours=2) > filled_orders[-1].order_filled_utc) or (slice_profit < -0.02))
@@ -28700,7 +28700,7 @@ class NostalgiaForInfinityX6(IStrategy):
 
     if (
       (self.grinding_v2_grind_3_enable)
-      and (is_derisk_1_found or is_derisk_2_found or is_derisk_3_found)
+      # and (is_derisk_1_found or is_derisk_2_found or is_derisk_3_found)
       and is_long_grind_entry
       and (current_time - timedelta(minutes=5) > filled_entries[-1].order_filled_utc)
       and ((current_time - timedelta(hours=2) > filled_orders[-1].order_filled_utc) or (slice_profit < -0.02))
@@ -50868,7 +50868,7 @@ class NostalgiaForInfinityX6(IStrategy):
 
     if (
       (self.grinding_v2_grind_1_enable)
-      and (is_derisk_1_found or is_derisk_2_found or is_derisk_3_found)
+      # and (is_derisk_1_found or is_derisk_2_found or is_derisk_3_found)
       and is_short_grind_entry
       and (current_time - timedelta(minutes=5) > filled_entries[-1].order_filled_utc)
       and ((current_time - timedelta(hours=2) > filled_orders[-1].order_filled_utc) or (slice_profit > 0.02))
@@ -50962,7 +50962,7 @@ class NostalgiaForInfinityX6(IStrategy):
 
     if (
       (self.grinding_v2_grind_2_enable)
-      and (is_derisk_1_found or is_derisk_2_found or is_derisk_3_found)
+      # and (is_derisk_1_found or is_derisk_2_found or is_derisk_3_found)
       and is_short_grind_entry
       and (current_time - timedelta(minutes=5) > filled_entries[-1].order_filled_utc)
       and ((current_time - timedelta(hours=2) > filled_orders[-1].order_filled_utc) or (slice_profit > 0.02))
@@ -51056,7 +51056,7 @@ class NostalgiaForInfinityX6(IStrategy):
 
     if (
       (self.grinding_v2_grind_3_enable)
-      and (is_derisk_1_found or is_derisk_2_found or is_derisk_3_found)
+      # and (is_derisk_1_found or is_derisk_2_found or is_derisk_3_found)
       and is_short_grind_entry
       and (current_time - timedelta(minutes=5) > filled_entries[-1].order_filled_utc)
       and ((current_time - timedelta(hours=2) > filled_orders[-1].order_filled_utc) or (slice_profit > 0.02))
