033e7458f292014d6f36e4f6faf8e23fccebe181
root <root@fraqtrade.home>
2025-07-29 16:00:09 +0300

    Updated strategy 2025-07-29 16:00:09

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -70,7 +70,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.5.292"
+    return "v16.5.293"
 
   stoploss = -0.99
 
@@ -724,6 +724,8 @@ class NostalgiaForInfinityX6(IStrategy):
       "stop_threshold_doom_futures",
       "stop_threshold_rapid_spot",
       "stop_threshold_rapid_futures",
+      "stop_threshold_scalp_spot",
+      "stop_threshold_scalp_futures",
       "derisk_enable",
       "stops_enable",
       "regular_mode_derisk_1_spot",
@@ -3725,6 +3727,15 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["AROONU_14_1h"] < 50.0)
         | (df["AROONU_14_4h"] < 85.0)
         | (df["ROC_9_4h"] < 30.0)
+      )
+      # 15m & 1h down move, 15m still high, 1h high & overbought
+      & (
+        (df["RSI_3_15m"] > 20.0)
+        | (df["RSI_3_1h"] > 60.0)
+        | (df["RSI_14_15m"] < 40.0)
+        | (df["RSI_14_1h"] < 50.0)
+        | (df["AROONU_14_1h"] < 70.0)
+        | (df["ROC_9_1h"] < 80.0)
       )
       # 15m & 1h down move, 15m still not low enough, 1h still high, 4h high & overbought
       & (
