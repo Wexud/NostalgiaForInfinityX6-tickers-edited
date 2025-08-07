99606aab1617c5248f0353ac032c8091dc391d50
root <root@fraqtrade.home>
2025-08-07 12:00:04 +0300

    Updated strategy 2025-08-07 12:00:04

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -70,7 +70,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.6.16"
+    return "v16.6.18"
 
   stoploss = -0.99
 
@@ -3997,6 +3997,17 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["AROONU_14_4h"] < 100.0)
         | (df["ROC_9_4h"] < 50.0)
       )
+      # 15m & 1h & 4h down move, 15m still not low enough, 1h still high, 4h high & overbought
+      & (
+        (df["RSI_3_15m"] > 25.0)
+        | (df["RSI_3_1h"] > 35.0)
+        | (df["RSI_3_4h"] > 40.0)
+        | (df["RSI_14_15m"] < 30.0)
+        | (df["RSI_14_1h"] < 40.0)
+        | (df["RSI_14_4h"] < 50.0)
+        | (df["AROONU_14_4h"] < 70.0)
+        | (df["ROC_9_4h"] < 80.0)
+      )
       # 15m & 1h down move, 15m still not low enough, 1h still high, 4h high, 4h & 1d overbought
       & (
         (df["RSI_3_15m"] > 25.0)
@@ -4179,6 +4190,8 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["STOCHRSIk_14_14_3_3_4h"] < 80.0)
         | (df["ROC_9_4h"] < 80.0)
       )
+      # 1h down move, 1h still high, 4h overbought
+      & ((df["RSI_3_1h"] > 50.0) | (df["RSI_14_1h"] < 40.0) | (df["ROC_9_4h"] < 250.0))
       # 1h & 1d down move, 1h & 4h & 1d still high, 1h & 4h high, 4h overbought
       & (
         (df["RSI_3_1h"] > 60.0)
