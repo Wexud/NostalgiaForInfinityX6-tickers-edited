71749d66b1a209ed30eca00aef956e5e6c8571a0
root <root@fraqtrade.home>
2025-09-15 12:00:05 +0300

    Updated strategy 2025-09-15 12:00:05

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -70,7 +70,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.7.93"
+    return "v16.7.95"
 
   stoploss = -0.99
 
@@ -5591,6 +5591,17 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["AROONU_14_1h"] < 90.0)
         | (df["ROC_9_4h"] < 10.0)
       )
+      # 15m & 1h down move, 15m & 1h still high, 4h high, 4h & 1d overbought
+      & (
+        (df["RSI_3_15m"] > 25.0)
+        | (df["RSI_3_1h"] > 50.0)
+        | (df["RSI_14_15m"] < 40.0)
+        | (df["RSI_14_1h"] < 50.0)
+        | (df["RSI_14_4h"] < 70.0)
+        | (df["AROONU_14_1h"] < 80.0)
+        | (df["ROC_9_4h"] < 20.0)
+        | (df["ROC_9_1d"] < 100.0)
+      )
       # 15m & 1h down move, 15m still high, 1h & 4h high, 1h overbought, 1d downtrend
       & (
         (df["RSI_3_15m"] > 25.0)
@@ -8103,10 +8114,10 @@ class NostalgiaForInfinityX6(IStrategy):
   # Correct Min Stake
   # ---------------------------------------------------------------------------------------------
   def correct_min_stake(self, min_stake: float) -> float:
-    # if self.config["exchange"]["name"] in ["bybit"]:
-    #   if self.is_futures_mode:
-    #     if min_stake < 5.0 / self.futures_mode_leverage:
-    #       min_stake = 5.0 / self.futures_mode_leverage
+    if self.config["exchange"]["name"] in ["bybit"]:
+      if self.is_futures_mode:
+        if min_stake < 5.0 / self.futures_mode_leverage:
+          min_stake = 5.0 / self.futures_mode_leverage
     return min_stake
 
   def is_backtest_mode(self) -> bool:
