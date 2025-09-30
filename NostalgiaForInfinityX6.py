f05fb947889bc5e193c8d8da12512ef6ddc39271
root <root@fraqtrade.home>
2025-09-30 16:00:07 +0300

    Updated strategy 2025-09-30 16:00:07

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -70,7 +70,7 @@ class NostalgiaForInfinityX6(IStrategy):
   INTERFACE_VERSION = 3
 
   def version(self) -> str:
-    return "v16.8.58"
+    return "v16.8.59"
 
   stoploss = -0.99
 
@@ -8272,6 +8272,16 @@ class NostalgiaForInfinityX6(IStrategy):
         | (df["RSI_14_4h"] > 70.0)
         | (df["CMF_20_15m"] < 0.25)
         | (df["STOCHRSIk_14_14_3_3_4h"] > 60.0)
+      )
+      # 15m & 1h & 4h up move, 15m still not high enough, 1h & 4h still low, 4h still low
+      & (
+        (df["RSI_3_15m"] < 90.0)
+        | (df["RSI_3_1h"] < 60.0)
+        | (df["RSI_3_4h"] < 60.0)
+        | (df["RSI_14_15m"] > 80.0)
+        | (df["RSI_14_1h"] > 60.0)
+        | (df["RSI_14_4h"] > 60.0)
+        | (df["AROONU_14_4h"] > 50.0)
       )
       # 15m & 1h & 4h up move, 15m & 1h still not high enough, 1h & 4h uptrend
       & (
